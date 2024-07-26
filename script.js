266ee4dbbb7482f1baea3be9333549eec3454e15
pragatmittal <143354114+pragatmittal@users.noreply.github.com>
2024-07-26 19:22:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var backToTopBtn = document.getElementById("backToTopBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
